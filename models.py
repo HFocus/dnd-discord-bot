2c87e9e765ad3b676753f85c9741cef22e376818
Keenan D. Buckley <keenandbuckley@protonmail.com>
2022-09-01 13:06:06 -0600

    Initial commit

---
+++ models.py
@@ -0,0 +1,47 @@
+'''
+Models for reading/writing data to database files
+Copyright (C) 2022 Keenan Buckley
+
+This file is part of {PROJECT_NAME}.
+
+{PROJECT_NAME} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+import dotenv
+from enum import Enum
+import os
+
+dotenv.load_dotenv("static/.env")  # load all the variables from the .env file
+
+class ENVs(Enum):
+    """Defines env variables that should be present in a .env file accessable to the script """
+    TOKEN = "TOKEN"
+    DEBUG_GUILD = "DEBUG_GUILD"
+    MONGODB_URI = "MONGODB_URI"
+    MONGODB_CERT_PATH = "MONGODB_CERT_PATH"
+
+
+def get_env_safe(key: ENVs):
+    """Get variable from .env and assert that it was found before resuming program
+
+    Args:
+        key (ENVs): enum associated to the enviromental variable
+
+    Returns:
+        value of the enviromental variable
+    """
+    value = os.getenv(key.value)
+    assert(
+        value is not None), f'Can\'t find token \"{key.value}\" and/or \".env\"'
+    return value
---
+++ run.py
@@ -0,0 +1,51 @@
+'''
+Entry point for python application. Run this file to start the bot.
+Copyright (C) 2022 Keenan Buckley
+
+This file is part of {PROJECT_NAME}.
+
+{PROJECT_NAME} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+import discord
+import logging
+import models
+
+logging.basicConfig(level=logging.INFO, filename='discord.log',
+                    filemode='w', format='date+time:%(asctime)s | %(message)s')
+DEBUG_GUILD = models.get_env_safe(models.ENVs.DEBUG_GUILD)
+bot = discord.Bot(debug_guilds=[DEBUG_GUILD])
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is ready and online!')
+    logging.info(f'{bot.user} is ready and online!')
+    logging.info(f'Connected to {len(bot.guilds)} Guilds:')
+    for guild in bot.guilds:
+        logging.info(f'{guild}|{guild.id}')
+
+
+def main():
+    # Run Pycord Bot until keyboard interrupt
+    logging.info(f'Starting Pycord Bot...')
+    bot.run(models.get_env_safe(models.ENVs.TOKEN))
+
+    # Safely shut down connections and save data
+    print(f'\nShutting down...')
+    logging.info(f'Shutting down...')
+
+
+if __name__ == '__main__':
+    main()
